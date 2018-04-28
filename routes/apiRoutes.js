27f90602aec8a53062f911133f9f673ef194109e
Joseph Lyford <lyford.joseph@yahoo.com>
2018-04-28 10:18:20 -0400

    table test

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,12 +14,12 @@ module.exports = function(app) {
   app.post('/api/tables', function(req, res){
     let newCustomer = req.body
     newCustomer.routeName = newCustomer.name.replace(/\s+/g, '').toLowerCase()
-    if(tables.length < 8) {
+    if(tables.length < 2) {
       tables.push(newCustomer)
       res.json(true)
     } else {
       reservations.push(req.newCustomer)
-      res.json(false)
+      res.json(true)
     }
     process.stdout.write('\x1B[2J\x1B[3J\x1B[H')
     console.log(tables)
